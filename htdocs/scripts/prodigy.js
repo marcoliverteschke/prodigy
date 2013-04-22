173fed0af13ae73ff7a15b0773db7c07b71c9ab5
Marc-Oliver Teschke <marc@Marc-Olivers-MacBook-Pro.local>
2013-04-22 21:08:38 +0200

    Added Handlebars.js, fixed many a path.

---
+++ htdocs/scripts/prodigy.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+//	var page_template_html = $('#page-template').html();
+//	var page_template = Handlebars.compile(page_template_html);
+//	$("body").prepend(page_template());
+});
