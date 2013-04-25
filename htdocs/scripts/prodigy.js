c5eefb05b356f49648d0d6ab41bff2d4231fbfb6
Marc-Oliver Teschke <marcoliverteschke@mac.com>
2013-04-25 10:08:56 +0200

    Added routing to switch between views.

--- htdocs/scripts/prodigy.js
+++ htdocs/scripts/prodigy.js
@@ -1,21 +1,27 @@
 var page_template_html;
 var page_template;
+var exercise_listitem_template_html;
+var exercise_listitem_template;
 
 $(document).ready(function(){
 	page_template_html = $('#page-template').html();
 	page_template = Handlebars.compile(page_template_html);
 	load_exercisetypes_list();
+	exercise_listitem_template_html = $('#exercise-listitem-template').html();
+	exercise_listitem_template = Handlebars.compile(exercise_listitem_template_html);
 });
 
 
 function load_exercises_list(exercisetype_id)
 {
 	remove_click_handler();
+	$('#exercises').empty();
 	return $.get('/api/exercises/' + exercisetype_id, function(data){
 		var content = '<ul data-role="listview">';
 		for(i in data)
 		{
-			content += '<li><a href="#exercises/' + data[i].id + '">' + data[i].name + '</a></li>';
+			var listitem_context = {'id' : data[i].id, 'name' : data[i].name, 'description' : data[i].description}
+			content += exercise_listitem_template(listitem_context);
 		}
 		content += '</ul>';
 		var context = {'title' : 'EXercises', 'content' : content}
@@ -29,11 +35,12 @@ function load_exercises_list(exercisetype_id)
 function load_exercisetypes_list()
 {
 	remove_click_handler();
+	$('#exercisetypes').empty();
 	$.get('/api/exercisetypes', function(data){
 		var content = '<ul data-role="listview" data-inset="true">';
 		for(i in data)
 		{
-			content += '<li><a href="#exercises" data-exercisetype="' + data[i].id + '">' + data[i].name + '</a></li>';
+			content += '<li><a href="#exercises/' + data[i].id + '">' + data[i].name + '</a></li>';
 		}
 		content += '</ul>';
 		var context = {'title' : 'PRodigy', 'content' : content}
@@ -58,17 +65,40 @@ function add_click_handler()
 
 		// Alter the url according to the anchor's href attribute, and
 		// store the data-foo attribute information with the url
-		if(load_exercises_list($(this).attr("data-exercisetype")))
-		{
-			$.mobile.navigate('#exercises', {
-	//			foo: this.attr("data-foo")
-			});
-		}
-
+		route($(this).attr('href'))
 		// Hypothetical content alteration based on the url. E.g, make
 		// an AJAX request for JSON data and render a template into the page.
 //		alterContent( this.attr("href") );
 	});
+}
 
+
+function route(hash)
+{
+	var hash_split = hash.split("/");
+	console.log(hash_split);
 	
+	switch (hash_split[0]) {
+		case "#exercisetypes":
+			$.mobile.navigate('#exercisetypes', {
+//						foo: this.attr("data-foo")
+			});
+			break;
+		case "#exercises":
+			if(typeof hash_split[1] != "undefined" && isNumber(hash_split[1]))
+			{
+				if(load_exercises_list(hash_split[1]))
+				{
+					$.mobile.navigate('#exercises', {
+//						foo: this.attr("data-foo")
+					});
+				}
+			}
+			break;
+	}
+}
+
+
+function isNumber(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
 }
